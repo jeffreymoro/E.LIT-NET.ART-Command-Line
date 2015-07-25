f17327bf63f4d3000dcb3c26f0324adec61170d8
jeffreymoro <jtm0624@me.com>
2015-07-25 18:09:00 -0400

    add Internet Arcade search
    prompts for game genre and searches Internet Arcade for it

--- prompt.js
+++ prompt.js
@@ -8,8 +8,16 @@ function prompt()
       };
 */
 
+// Get a game from the Internet Archive
+function gamePrompt(){
+  var gameSearch = prompt("What game would you like to play?");
+  window.location.href = "https://archive.org/details/internetarcade?and[]=" + gameSearch.toLowerCase();
+};
+
+// Hey it's a little text adventure right here!
+
 // Link to the specific page. [Revised w/ easter eggs]
-function prompt(){
+function commandPrompt(){
   var promptElement = document.getElementById("input");
   var promptInput = promptElement.value;
   var promptInput = promptInput.toLowerCase();
@@ -23,11 +31,20 @@ function prompt(){
     window.location.href = "http://elit.jeffreymoro.com/about";
   } else if (promptInput === "otters"){
     window.location.href = "https://www.youtube.com/watch?v=epUk3T2Kfno";
-  } else if (promptInput === "five college digital humanities" || "5colldh"){
+  } else if (promptInput === "five college digital humanities" || promptInput === "5colldh"){
     window.location.href = "http://5colldh.org";
+  } else if (promptInput === "game") {
+    // This is where the text adventure will go, when I finally get it working.
+    if (window.screen.width > 900){
+        gamePrompt();
+    } else {
+      window.location.href = "https://archive.org/details/internetarcade"
+    };
+  } else if (promptInput === "secrets") {
+    alert("Wouldn't you like to know?");
   } else {
     alert("I'm afraid I can't do that.");
-  };
+  }
 };
 
 // Let you type wherever!
@@ -41,7 +58,7 @@ jQuery(document).ready(function()
 {
     jQuery(document).keypress(function(e) {
         if(e.which == 13) {
-            prompt();
+            commandPrompt();
         }
     });
 });
